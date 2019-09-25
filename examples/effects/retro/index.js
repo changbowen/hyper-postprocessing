0f2da7c57f36f91a4fb5d5bd7d92074954b5d7fc
Carl Chang <changb0wen@outlook.com>
2019-09-25 21:13:30 +0800

    add an fps option; retro animation tweaks;

--- examples/effects/retro/index.js
+++ examples/effects/retro/index.js
@@ -23,7 +23,7 @@ const savePass = new POSTPROCESSING.SavePass(saveTarget)
 const burnInEffect = new GlslEffect('burn-in', { 
 		uniforms: new Map([
 			[ 'burnInSource', new THREE.Uniform(savePass.renderTarget.texture)], 
-			[ 'burnInTime', new THREE.Uniform(0.3)], 
+			[ 'burnInTime', new THREE.Uniform(0.2)], 
 		]),
 	}
 );
@@ -33,19 +33,19 @@ const screenCurvature = 0.2;
 
 const retroEffect = new GlslEffect('retro', {
 		uniforms: new Map([
-			[ 'fontColor', new THREE.Uniform(new THREE.Vector3(500/255, 250/255, 150/255))], 
+			[ 'fontColor', new THREE.Uniform(new THREE.Vector3(255/255, 140/255, 80/255))], 
 			// [ 'backgroundColor', new THREE.Uniform(new THREE.Vector3(0.1, 0.0, 0.0))], 
 			[ 'chromaColor', new THREE.Uniform(0.5)], 
 			// [ 'staticNoise', new THREE.Uniform(0.2)], 
 			[ 'noiseSource', new THREE.Uniform(null)], 
 			// [ 'horizontalSyncStrength', new THREE.Uniform(0.05)], 
 			// [ 'horizontalSyncFrequency', new THREE.Uniform(0.30)], 
-			// [ 'jitter', new THREE.Uniform(new THREE.Vector2(0.007 * jitter, 0.002 * jitter))], 
+			[ 'jitter', new THREE.Uniform(new THREE.Vector2(0.007 * jitter, 0.002 * jitter))], 
 			[ 'glowingLine', new THREE.Uniform(0.4)], 
 			// [ 'flickering', new THREE.Uniform(0.2)], 
 			// [ 'ambientLight', new THREE.Uniform(0.05)], 
-			[ 'pixelHeight', new THREE.Uniform(14)], 
-			[ 'pixelization', new THREE.Uniform(true)], 
+			[ 'pixelHeight', new THREE.Uniform(13)], 
+			// [ 'pixelization', new THREE.Uniform(true)], 
 			// [ 'rbgSplit', new THREE.Uniform(0.2)], 
 		]),
 	}
@@ -82,19 +82,27 @@ function coordinateTransform(x, y) {
 	return [x * dist + x + 0.5, y * dist + y + 0.5];
 };
 
-
+// antialiasing
 const searchImage = new Image(); searchImage.src = POSTPROCESSING.SMAAEffect.searchImageDataURL;
 const areaImage = new Image(); areaImage.src = POSTPROCESSING.SMAAEffect.areaImageDataURL;
 const smaaEffect = new POSTPROCESSING.SMAAEffect(searchImage, areaImage);
+
+// brightness
+const brightEffect = new POSTPROCESSING.BrightnessContrastEffect({
+	brightness: 0.16,
+	contrast: 0.35
+})
+
 
 return {
 	passes: [
 		new POSTPROCESSING.EffectPass(null, burnInEffect),
 		savePass,
 		new POSTPROCESSING.EffectPass(null, retroEffect),
+		new POSTPROCESSING.EffectPass(null, brightEffect),
 		new POSTPROCESSING.EffectPass(null, bloomEffect),
 		new POSTPROCESSING.EffectPass(null, frameEffect),
-		new POSTPROCESSING.EffectPass(null, smaaEffect),
+		// new POSTPROCESSING.EffectPass(null, smaaEffect),
 	],
 	coordinateTransform: coordinateTransform,
 };
