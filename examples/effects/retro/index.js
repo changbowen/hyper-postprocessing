60930e5b40cf29cbbb984765712770ef7777fa0b
Carl Chang <changb0wen@outlook.com>
2019-09-25 16:20:45 +0800

    add / remove a few effects for a better look

--- examples/effects/retro/index.js
+++ examples/effects/retro/index.js
@@ -23,30 +23,30 @@ const savePass = new POSTPROCESSING.SavePass(saveTarget)
 const burnInEffect = new GlslEffect('burn-in', { 
 		uniforms: new Map([
 			[ 'burnInSource', new THREE.Uniform(savePass.renderTarget.texture)], 
-			[ 'burnInTime', new THREE.Uniform(0.4)], 
+			[ 'burnInTime', new THREE.Uniform(0.3)], 
 		]),
 	}
 );
 
-const jitter = 0.3;
+const jitter = 0.2;
 const screenCurvature = 0.2;
 
 const retroEffect = new GlslEffect('retro', {
 		uniforms: new Map([
-			[ 'fontColor', new THREE.Uniform(new THREE.Vector3(200/255, 255/255, 110/255))], 
-			[ 'backgroundColor', new THREE.Uniform(new THREE.Vector3(0.0, 0.05, 0.0))], 
-			[ 'chromaColor', new THREE.Uniform(1.0)], 
-			[ 'staticNoise', new THREE.Uniform(0.2)], 
+			[ 'fontColor', new THREE.Uniform(new THREE.Vector3(500/255, 250/255, 150/255))], 
+			// [ 'backgroundColor', new THREE.Uniform(new THREE.Vector3(0.1, 0.0, 0.0))], 
+			[ 'chromaColor', new THREE.Uniform(0.5)], 
+			// [ 'staticNoise', new THREE.Uniform(0.2)], 
 			[ 'noiseSource', new THREE.Uniform(null)], 
-			[ 'horizontalSyncStrength', new THREE.Uniform(0.250)], 
-			[ 'horizontalSyncFrequency', new THREE.Uniform(0.30)], 
-			[ 'jitter', new THREE.Uniform(new THREE.Vector2(0.007 * jitter, 0.002 * jitter))], 
+			// [ 'horizontalSyncStrength', new THREE.Uniform(0.05)], 
+			// [ 'horizontalSyncFrequency', new THREE.Uniform(0.30)], 
+			// [ 'jitter', new THREE.Uniform(new THREE.Vector2(0.007 * jitter, 0.002 * jitter))], 
 			[ 'glowingLine', new THREE.Uniform(0.4)], 
-			[ 'flickering', new THREE.Uniform(0.2)], 
-			[ 'ambientLight', new THREE.Uniform(0.05)], 
-			[ 'pixelHeight', new THREE.Uniform(8.0)], 
-			[ 'pixelization', new THREE.Uniform(false)], 
-			[ 'rbgSplit', new THREE.Uniform(0.2)], 
+			// [ 'flickering', new THREE.Uniform(0.2)], 
+			// [ 'ambientLight', new THREE.Uniform(0.05)], 
+			[ 'pixelHeight', new THREE.Uniform(14)], 
+			[ 'pixelization', new THREE.Uniform(true)], 
+			// [ 'rbgSplit', new THREE.Uniform(0.2)], 
 		]),
 	}
 );
@@ -59,14 +59,14 @@ new THREE.TextureLoader().load(resolve(__dirname, '../../images/allNoise512.png'
 });
 
 const bloomEffect = new POSTPROCESSING.BloomEffect({
-	kernelSize: 3,
-	distinction: 0.2,
-	// blendFunction: POSTPROCESSING.BlendFunction.AVERAGE,
+	kernelSize: 4,
+	distinction: -0.4,
+	blendFunction: POSTPROCESSING.BlendFunction.ADD,
 });
 
 const frameEffect = new GlslEffect('retro_frame', {
 		uniforms: new Map([
-			[ 'frameColor', new THREE.Uniform(new THREE.Vector3(245/255, 238/255, 216/255))], 
+			[ 'frameColor', new THREE.Uniform(new THREE.Vector3(150/255, 80/255, 60/255))], 
 			[ 'screenCurvature', new THREE.Uniform(screenCurvature)], 
 		]),
 	}
@@ -82,18 +82,21 @@ function coordinateTransform(x, y) {
 	return [x * dist + x + 0.5, y * dist + y + 0.5];
 };
 
-return { 
+
+const searchImage = new Image(); searchImage.src = POSTPROCESSING.SMAAEffect.searchImageDataURL;
+const areaImage = new Image(); areaImage.src = POSTPROCESSING.SMAAEffect.areaImageDataURL;
+const smaaEffect = new POSTPROCESSING.SMAAEffect(searchImage, areaImage);
+
+return {
 	passes: [
 		new POSTPROCESSING.EffectPass(null, burnInEffect),
 		savePass,
 		new POSTPROCESSING.EffectPass(null, retroEffect),
 		new POSTPROCESSING.EffectPass(null, bloomEffect),
 		new POSTPROCESSING.EffectPass(null, frameEffect),
+		new POSTPROCESSING.EffectPass(null, smaaEffect),
 	],
 	coordinateTransform: coordinateTransform,
 };
 
-
 };
-
-
